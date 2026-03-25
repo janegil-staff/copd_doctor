e70bc1b032b12e3f46cf9177b7cc2b5fda9d18fd
janegil-staff <janstovr@icloud.com>
2026-03-25 19:52:55 +0100

    Changed page title to show COPT Calendar if url is copdcalendar.com, if else ithe title will say Kolskalendar, and description also takes language in consideration

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LangProvider } from "@/context/LangContext";
+import { headers } from "next/headers";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({
@@ -9,10 +10,19 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  title: "Kolskalendar",
-  description: "COPD symptom calendar",
-};
+export async function generateMetadata() {
+  const headersList = await headers();
+  const host = headersList.get("host") ?? "";
+
+  const isEnglish = host.includes("copdcalendar.com");
+
+  return {
+    title: isEnglish ? "COPD Calendar" : "Kolskalendar",
+    description: isEnglish
+      ? "COPD symptom calendar"
+      : "Symptomkalender for kols",
+  };
+}
 
 export default function RootLayout({ children }) {
   return (
